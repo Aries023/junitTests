6a741ad1a76e8ddabf450874380b80ecd11f3cea
Aries023 <pet.potocka@gmail.com>
2022-11-21 05:21:39 +0100

    - add throw illegalArgumentException, into degree method.

--- src/main/java/com/pet/potocka/junitTests/PieceOfCode/SimpleCalculator.java
+++ src/main/java/com/pet/potocka/junitTests/PieceOfCode/SimpleCalculator.java
@@ -9,23 +9,29 @@ public class SimpleCalculator {
 
     // this method give you degree (for example if you pass exam (F means you failed, A means you are the best))
     public char degree (int arg){
-        if (arg < 60){
-            return 'F';
-        }else {
-            if (arg < 70){
-                return 'D';
+        if (arg < 0 || arg > 100){
+            throw new IllegalArgumentException("Degree number is under 0, or upper 100");
+        }
+        else {
+            if (arg < 60){
+                return 'F';
             }else {
-                if (arg<80){
-                    return 'C';
+                if (arg < 70){
+                    return 'D';
                 }else {
-                    if (arg<90){
-                        return 'B';
-                    }
-                    else {
-                        return 'A';
+                    if (arg<80){
+                        return 'C';
+                    }else {
+                        if (arg<90){
+                            return 'B';
+                        }
+                        else {
+                            return 'A';
+                        }
                     }
                 }
             }
         }
+
     }
 }
